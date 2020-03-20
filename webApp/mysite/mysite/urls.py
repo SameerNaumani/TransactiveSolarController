1b99e8b76e588ad607b8aa476ce555f890ed383a
SameerNaumani <sameernaumani@Sauds-MacBook-Pro.local>
2020-03-20 16:19:56 -0400

    update views.py

--- webApp/mysite/mysite/urls.py
+++ webApp/mysite/mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    # path() passes four arguments, two required: route and view. Optional: kwargs and name
+    path('bids/', include('bids.urls')),
     path('admin/', admin.site.urls),
 ]
